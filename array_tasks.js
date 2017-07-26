4d768f8b46d581fdff3defa274f5afa8d715cd10
Jamie Martin <jamiemartinmusic@gmail.com>
2017-07-26 17:29:12 +0100

    should concat two arrays passed

--- array_tasks.js
+++ array_tasks.js
@@ -1,8 +1,15 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
-
-	// },
+	concat: function (arr1, arr2) {
+  	var tempArray = [];
+  	for (var item of arr1){
+  		tempArray.push(item)
+  	}
+  	for (var item of arr2){
+  		tempArray.push(item)
+  	}
+  	return tempArray;
+	},
 
 	// insertAt: function (arr, itemToAdd, index) {
 		
